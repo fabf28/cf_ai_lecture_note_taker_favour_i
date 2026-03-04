7cdd6f1bded3bba16773c2fde88cefb7096b71cb
fabf28 <ifavour228@gmail.com>
2026-03-04 15:35:00 -0700

    workflow template

--- workflow/src/index.ts
+++ workflow/src/index.ts
@@ -11,8 +11,25 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export { MyWorkflow } from "./workflow";
+
+
+import { WorkerEntrypoint } from "cloudflare:workers";
+
+export default class WorkflowsService extends WorkerEntrypoint {
+	// Currently, entrypoints without a named handler are not supported
+	async fetch() {
+		return new Response(null, { status: 404 });
+	}
+
+	async createInstance(payload: any) {
+		let instance = await this.env.MY_WORKFLOW.create({
+			params: payload,
+		});
+
+		return Response.json({
+			id: instance.id,
+			details: await instance.status(),
+		});
+	}
+}
