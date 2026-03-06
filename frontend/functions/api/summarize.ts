ba64c001b0209168b0fe2650e49a1945249e698e
fabf28 <ifavour228@gmail.com>
2026-03-06 08:40:52 -0700

    add status get method in pages func

--- frontend/functions/api/summarize.ts
+++ frontend/functions/api/summarize.ts
@@ -13,3 +13,12 @@ export const onRequestPost = async ({ request }) => {
 
     return response;
 };
+
+export const onRequestGet = async ({ request }) => {
+    // This payload could be anything from within your app or from your frontend
+    const url = new URL(request.url);
+    const id = url.searchParams.get("id");
+    const newUrl = "http://127.0.0.1:8787/?instanceId=" + id;
+    const response = await fetch(newUrl);
+    return response;
+};
