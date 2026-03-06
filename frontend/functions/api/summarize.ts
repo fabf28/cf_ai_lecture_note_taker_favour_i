0815cff57108fc6faf23aff8e4cb2f0a4709605d
fabf28 <ifavour228@gmail.com>
2026-03-06 08:21:49 -0700

    create workflow, add ai bindings, pages function

--- frontend/functions/api/summarize.ts
+++ frontend/functions/api/summarize.ts
@@ -1,4 +1,15 @@
-export function onRequestPost() {
-    const notes = "These are your notes.";
-    return Response.json(notes);
-}
+export const onRequestPost = async ({ request }) => {
+    // This payload could be anything from within your app or from your frontend
+    const form = await request.formData();
+    const audio = form.get("audio");
+
+    const forwardForm = new FormData();
+    forwardForm.append("audio", audio, "recording.webm");
+
+    const response = await fetch("http://127.0.0.1:8787/", {
+        method: "POST",
+        body: forwardForm
+    });
+
+    return response;
+};
--- lecture-ai-workflow/src/index.ts
+++ lecture-ai-workflow/src/index.ts
@@ -13,11 +13,11 @@ import {
  *
  * Learn more at https://developers.cloudflare.com/workflows
  */
- 
+
 // User-defined params passed to your Workflow
 type Params = {
-	email: string;
-	metadata: Record<string, string>;
+	audio: any,
+	type: any
 };
 
 export class MyWorkflow extends WorkflowEntrypoint<Env, Params> {
@@ -25,54 +25,44 @@ export class MyWorkflow extends WorkflowEntrypoint<Env, Params> {
 		// Can access bindings on `this.env`
 		// Can access params on `event.payload`
 
-		const files = await step.do("my first step", async () => {
+		//step 1 - transcribe audio
+		const transcript = await step.do("my first step", async () => {
 			// Fetch a list of files from $SOME_SERVICE
-			return {
-				inputParams: event,
-				files: [
-					"doc_7392_rev3.pdf",
-					"report_x29_final.pdf",
-					"memo_2024_05_12.pdf",
-					"file_089_update.pdf",
-					"proj_alpha_v2.pdf",
-					"data_analysis_q2.pdf",
-					"notes_meeting_52.pdf",
-					"summary_fy24_draft.pdf",
-				],
+			console.log(event.payload);
+			const inputs = {
+				audio: event.payload.audio
 			};
-		});
-
-		// You can optionally have a Workflow wait for additional data,
-		// human approval or an external webhook or HTTP request, before progressing.
-		// You can submit data via HTTP POST to /accounts/{account_id}/workflows/{workflow_name}/instances/{instance_id}/events/{eventName}
-		const waitForApproval = await step.waitForEvent("request-approval", {
-			type: "approval", // define an optional key to switch on
-			timeout: "1 minute", // keep it short for the example!
-		});
-
-		const apiResponse = await step.do("some other step", async () => {
-			let resp = await fetch("https://api.cloudflare.com/client/v4/ips");
-			return await resp.json<any>();
+			const response = await this.env.AI.run('@cf/openai/whisper', inputs);
+			return {
+				text: response.text,
+			};
 		});
 
 		await step.sleep("wait on something", "1 minute");
 
+		//step 2 - generate 
 		await step.do(
-			"make a call to write that could maybe, just might, fail",
-			// Define a retry strategy
-			{
-				retries: {
-					limit: 5,
-					delay: "5 second",
-					backoff: "exponential",
-				},
-				timeout: "15 minutes",
-			},
+			"turn text into notes",
 			async () => {
-				// Do stuff here, with access to the state from our previous steps
-				if (Math.random() > 0.5) {
-					throw new Error("API call to $STORAGE_SYSTEM failed");
-				}
+				const messages = [
+					{
+						role: "system",
+						content: 'You are note maker. You are creating taking in text that is a lecture transcript. You will first make sense of the transcript, then you will turn it into keyword/phrases matched with definitions. You will then summaraize that into notes. Return the following json structure for the notes: {"notes":[{"phrase": <the phrase>, "definition" : <the defenition>}, {"phrase": <the phrase>, "definition" : <the defenition>}, {"phrase": <the phrase>, "definition" : <the defenition>}, ...], "summary" : <the summary>}'
+
+					},
+					{
+						role: "user",
+						content: transcript.text,
+					},
+				];
+
+				const inputs = {
+					messages: messages
+				};
+
+				const value = await this.env.AI.run("@cf/meta/llama-3.3-70b-instruct-fp8-fast", inputs);
+				console.log(value);
+				return Response.json(value);
 			},
 		);
 	}
@@ -91,12 +81,22 @@ export default {
 		if (id) {
 			let instance = await env.MY_WORKFLOW.get(id);
 			return Response.json({
-				status: await instance.status(),
+				details: await instance.status(),
 			});
 		}
 
 		// Spawn a new instance and return the ID and status
-		let instance = await env.MY_WORKFLOW.create();
+
+		const form = await req.formData();
+		const audio = form.get("audio");
+		const buffer = await audio!.arrayBuffer();
+
+		const instance = await env.MY_WORKFLOW.create({
+			params: {
+				audio: Array.from(new Uint8Array(buffer)),
+				type: audio!.type
+			}
+		});
 		// You can also set the ID to match an ID in your own system
 		// and pass an optional payload to the Workflow
 		// let instance = await env.MY_WORKFLOW.create({
