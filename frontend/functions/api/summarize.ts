de3b444e51ae3f5f5038b4ce3ac95453f3dae296
fabf28 <ifavour228@gmail.com>
2026-08-05 13:10:25 -0600

    feat: add host env variables

--- frontend/functions/api/summarize.ts
+++ frontend/functions/api/summarize.ts
@@ -1,16 +1,19 @@
-export const onRequestPost = async ({ request }) => {
-    return fetch("http://lecture-ai-workflow.favour-iheanyichukwu.workers.dev/", {
+
+export const onRequestPost = async ({ request, env }: { request: any; env: any }) => {
+    const host = env.WORKER_HOST || "http://127.0.0.1:8787";
+    return fetch(`${host}/`, {
         method: "POST",
         body: request.body,
         headers: request.headers
     });
 };
 
-export const onRequestGet = async ({ request }) => {
+export const onRequestGet = async ({ request, env }: { request: any; env: any }) => {
+    const host = env.WORKER_HOST || "http://127.0.0.1:8787";
     // This payload could be anything from within your app or from your frontend
     const url = new URL(request.url);
     const id = url.searchParams.get("id");
-    const newUrl = "http://lecture-ai-workflow.favour-iheanyichukwu.workers.dev/?instanceId=" + id;
+    const newUrl = `${host}/?instanceId=${id}`;
     const response = await fetch(newUrl);
     return response;
 };
